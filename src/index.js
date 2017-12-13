db5c32a6dce42e6ecd9f693455795b25b6a4bc94
Jason Hughes <jasonhughes088@gmail.com>
2017-12-12 18:57:16 -0700

    our add movies is showing as an empty array in store

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import rootReducers from './rootReducers';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-const store = createStore(rootReducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(rootReducers, devTools)
 
 render(
   <Provider store={ store } >
