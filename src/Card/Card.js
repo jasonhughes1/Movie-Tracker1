f80a9a8e3d8c37b1181bf92fdecd1817b5e0ba9f
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2017-12-13 20:05:15 -0700

    render movies

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div>jdfkfjdsal</div>
+    <div>
+      <h1>{props.title}</h1>
+      <p>{props.overview}</p>
+      <img src={`https://image.tmdb.org/t/p/w500${props.poster}`} />
+      <img src={`https://image.tmdb.org/t/p/w500${props.backdrop}`} />
+      <p>{props.vote}</p>
+
+    </div>
   )
 }
 
