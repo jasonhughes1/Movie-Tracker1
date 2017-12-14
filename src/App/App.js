2f833e2ec5aea3b919cc7a1222c7124719e53d2f
Jason Hughes <jasonhughes088@gmail.com>
2017-12-14 09:59:25 -0700

    setup files for login and register

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import CardList from '../CardList/CardList';
 import NavBar from '../NavBar/NavBar';
 import { Route } from 'react-router-dom';
+import Login from '../Login/Login.js';
+import Register from '../Register/Register.js'
 
 class App extends Component {
 
@@ -11,9 +13,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList />
         <Route path='/' component={NavBar} key='NavBar'/>
-        <Route exact path='/cardlist' component={CardList} key='CardList'/>
+        <Route exact path='/' component={CardList} key='CardList'/>
+        <Route exact path='/login' component={Login} key='login' />
+        <Route exact path='/register' component={Register} key='login' />
         <h1 className="App-title">MovieTracker</h1>
       </div>
     );
---
+++ src/Login/Login.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+export default class Login extends Component {
+  constructor() {
+    super();
+  }
+
+
+  render() {
+    return(
+      <div>This is the login page</div>
+    )
+  }
+}
