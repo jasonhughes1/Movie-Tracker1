4387c142a65e7c445ceaa26754425a42d34b5cd2
Pat Neel <patrick.neel@colorado.edu>
2017-12-16 15:54:15 -0700

    initally set up favorites

--- src/API/helper.js
+++ src/API/helper.js
@@ -47,6 +47,8 @@ async function fetchMovies(){
     }
   }
 
+  
 
 
-  export default { fetchMovies, userLogin };
+
+  export default { fetchMovies, userLogin};
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import './Card.css'
+import './Card.css';
+
+
 
 const Card = (props) => {
+  
   return (
     <div className = 'card'>
       <h1 className = 'title'>{props.title}</h1>
       <p className = 'overview'>{props.overview}</p>
       <div className = 'image-container'>
-        <button onClick={() => console.log('hola')}
+        <button onClick={() => props.favorites(props.title)}
           className = 'favorite'></button>
         <img className = 'poster' src={`https://image.tmdb.org/t/p/w500${props.poster}`} />
       </div>
