48c21e94e0860fe1f968eb54ba0c621d91251cce
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2017-12-18 10:50:23 -0700

    style backdrop

--- src/Components/Backdrop/Backdrop.js
+++ src/Components/Backdrop/Backdrop.js
@@ -6,7 +6,7 @@ const Backdrop = (props) => {
 
   return (
     <div className = 'backdrop-container'>
-      <h3>MOVIE<span>TRACKER</span></h3>
+      <h3>MOVIE<span> TRACKER</span></h3>
       <img className = 'backdrop' src={`https://image.tmdb.org/t/p/w500${props.backdrop}`} />
     </div>
   )
