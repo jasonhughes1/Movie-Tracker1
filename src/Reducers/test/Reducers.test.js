31e9f6c5fbd073b6dc6717a7d7a56292f67963d6
Jason Hughes <jasonhughes088@gmail.com>
2017-12-18 08:43:10 -0700

    actions tests added

--- src/Reducers/test/Reducers.test.js
+++ src/Reducers/test/Reducers.test.js
@@ -15,4 +15,11 @@ it('should return a new state with movies added', () => {
   const expected = [movie]
   expect(addMoviesReducer(undefined, actions.addMovies(movie))).toEqual(expected)
   })
+
+it.skip('should return a new state with favorite removed', () => {
+  const currentState = [{title: "Justice League", poster: "/Zz.jpg", vote: 6.6, overview: "Fueled.", backdrop: "/o5T8MQB.jpg", id: 89090}]
+  const expected = [];
+
+  expect(favorite(currentState, actions.setFavorites(89090))).toEqual(expected)
+})
 });
